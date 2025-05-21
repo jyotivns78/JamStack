cab81a2c1980b98e93cb7b580ba3cb51b8d21554
jyoti(jyoti.vishwakarma@langoor.com) <165015828+jyoti-vishwakarma@users.noreply.github.com>
2025-05-21 21:06:54 +0530

    Descriptive ENV File

--- src/_data/news.js
+++ src/_data/news.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 module.exports = async function() {
     try {
-      const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.API_KEY}&pageSize=5`);
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     //   console.log(response);
     // return response.data.articles;
     return response.data;
