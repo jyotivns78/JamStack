8ab43753663a363a2e51f864f467e6c62d412062
jyoti(jyoti.vishwakarma@langoor.com) <165015828+jyoti-vishwakarma@users.noreply.github.com>
2025-05-21 21:03:41 +0530

    Adding Dynamic

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,8 @@ module.exports = function(config) {
   return {
     dir: {
       input: "src",
-      output: "dist"
+      output: "dist",
+      data: "_data",
     }
   }
 }
---
+++ src/_data/news.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+require('dotenv').config();
+
+module.exports = async function() {
+    try {
+      const response = await axios.get(`https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${process.env.API_KEY}&pageSize=5`);
+    //   console.log(response);
+    // return response.data.articles;
+    return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
